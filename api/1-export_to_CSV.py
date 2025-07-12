7e6239a2ecc91b741f8d1933242f27176468e10c
christian-regnante <c.ngwino@alustudent.com>
2025-07-12 21:35:44 +0200

    Export to csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,24 +1,73 @@
-#!/usr/bin/python3
-''' Test request to parse API's
-'''
+#!/usr/bin/env python3
+
+"""
+Gather data from APIs and save them into a CSV file format
+"""
+
+
 import csv
 import requests
+from requests.exceptions import RequestException
 import sys
 
+
+def user_employee(user_Id):
+    try:
+        user_url = "https://jsonplaceholder.typicode.com/users"
+        user_request = requests.get(user_url + "/" + user_Id)
+        user_request.raise_for_status()
+        user_response = user_request.json()
+    except RequestException as e:
+        print(f"User can not be found,\n{e}")
+
+    employee_name = user_response.get('username')
+    employee_id = int(user_Id)
+
+    employee_todos(employee_name, employee_id)
+
+
+def employee_todos(Emp_name, Emp_id):
+    try:
+        todos_url = "https://jsonplaceholder.typicode.com/todos"
+        todos_request = requests.get(todos_url)
+        todos_request.raise_for_status()
+        todos_response = todos_request.json()
+    except RequestException as e:
+        print(f"Todos not found,\n{e}")
+
+    todos_data = []
+    for count in todos_response:
+        if count.get('userId') == Emp_id:
+            todos_data.append(count)
+
+    csv_formatting(Emp_name, todos_data)
+
+
+def csv_formatting(Emp_name, todos_data):
+    with open('USER_ID.csv', 'w', newline='') as user_file:
+        fieldnames = ['USER_ID', 'USERNAME',
+                      'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+
+        thewriter = csv.DictWriter(user_file, fieldnames=fieldnames)
+
+        thewriter.writeheader()
+
+        for tasks in todos_data:
+            thewriter.writerow({
+                'USER_ID': tasks.get('userId'),
+                'USERNAME': Emp_name,
+                'TASK_COMPLETED_STATUS': tasks.get('completed'),
+                'TASK_TITLE': tasks.get('title')
+            })
+
+        if thewriter:
+            print(f"{user_file.name} was created successfuly!!")
+
+
+def main():
+    Emp_ID = sys.argv[1]
+    user_employee(Emp_ID)
+
+
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1].isdigit():
-        api_endpoint = "https://jsonplaceholder.typicode.com"
-        user_id = sys.argv[1]
-        user_data = requests.get(api_endpoint + "/users/" + user_id).json()
-        username = user_data.get('username')
-        todo_data = \
-            requests.get(api_endpoint + "/users/" + user_id + "/todos").\
-            json()
-        with open("{}.csv".format(user_id), 'w') as csv_file:
-            for task in todo_data:
-                csv_file.write('"{}","{}","{}","{}"\n'.format(
-                    user_id,
-                    username,
-                    task.get('completed'),
-                    task.get('title')
-                ))
+    main()
